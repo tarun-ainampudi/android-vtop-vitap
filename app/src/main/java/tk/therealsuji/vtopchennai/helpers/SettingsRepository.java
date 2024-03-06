d81211ffbf547ca16daf5dd309264555aa4b57b3
sanjay7178 <saisanjay7660@gmail.com>
2024-03-07 01:48:28 +0530

    Revert "Added new api schemas"
    This reverts commit 8981a4066f047d45bd21af0b7a8a9fc43be6a1fe.

--- app/src/main/java/tk/therealsuji/vtopchennai/helpers/SettingsRepository.java
+++ app/src/main/java/tk/therealsuji/vtopchennai/helpers/SettingsRepository.java
@@ -81,8 +81,8 @@ public class SettingsRepository {
     public static final String MOODLE_LOGIN_PATH = "/login/token.php";
     public static final String MOODLE_UPLOAD_PATH = "/webservice/upload.php";
     public static final String MOODLE_WEBSERVICE_PATH = "/webservice/rest/server.php";
-// TODO : VTOP LINK
-    public static final String VTOP_BASE_URL = "https://vtop.vitap.ac.in/vtop";
+
+    public static final String VTOP_BASE_URL = "https://vtopcc.vit.ac.in/vtop";
 
     public static final int THEME_DAY = 1;
     public static final int THEME_NIGHT = 2;
